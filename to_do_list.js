d38b22e1cce0823ae83b266e2724c9be0be12de6
YuKi-K-github <yuki.tennis2448@gmail.com>
2021-07-28 06:38:07 +0900

    radio function

--- to_do_list.js
+++ to_do_list.js
@@ -5,19 +5,19 @@
   const todos = [];
 
   add_Button.addEventListener('click', (event) => {
-    const todo = { comment: input_Text.value, status: '作業中'}
+    const todo = { id: todos.length, comment: input_Text.value, status: '作業中'}
     input_Text.focus();
     if (input_Text.value) {
       todos.push(todo);
       input_Text.value = '';
-      showTodos();
+      radioChange()
     }
   });
 
-  const showTodos = () => {
+  const showTodos = (todos) => {
     table_Body.textContent = '';
 
-    todos.forEach((todo, number) => {
+    todos.forEach((todo) => {
       const tableRecord = document.createElement('tr');
       table_Body.appendChild(tableRecord);
 
@@ -26,7 +26,7 @@
       const tableStatus = document.createElement('td');
       const tableDelete = document.createElement('td');
 
-      tableId.textContent = number;
+      tableId.textContent = todo.id;
       tableComment.textContent = todo.comment;
 
       tableRecord.appendChild(tableId);
@@ -34,31 +34,54 @@
       tableRecord.appendChild(tableStatus);
       tableRecord.appendChild(tableDelete);
 
-      tableStatus.appendChild(createStatusButton());
-      tableDelete.appendChild(createDeleteButton(tableRecord));
+      tableStatus.appendChild(createStatusButton(todo));
+      tableDelete.appendChild(createDeleteButton(todo.id));
     });
   };
 
-  const createStatusButton = () => {
+  const createStatusButton = (todo) => {
     const statusButton = document.createElement('button');
-    statusButton.textContent = '作業中';
+    statusButton.textContent = todo.status;
     statusButton.addEventListener('click', () => {
-      if (statusButton.textContent === '作業中') {
-        statusButton.textContent = '完了';
+      if (todo.status === '作業中') {
+        todo.status = '完了';
       } else {
-        statusButton.textContent = '作業中';
+        todo.status = '作業中';
       }
+      showTodos(todos);
     });
     return statusButton;
   };
 
-const createDeleteButton = (tableRecord) => {
-  let index = tableRecord.rowIndex-1;
+const createDeleteButton = (id) => {
   const deleteButton = document.createElement('button');
   deleteButton.textContent = '削除';
   deleteButton.addEventListener('click', () => {
-    todos.splice(index, 1);
-    showTodos();
+    todos.splice(id, 1);
+    showTodos(todos);
+    todos.reduce((Idnum, todo) => (todo.id = Idnum + 1), -1);
+    showTodos(todos);
   });
   return deleteButton;
 };
+
+function radioChange() {
+  const radioButton_All = document.getElementById('radio-all');
+  const radioButton_Working = document.getElementById('radio-work');
+  const radioButton_Done = document.getElementById('radio-done');
+
+  if (radioButton_All.checked) {
+    todos.slice();
+    return showTodos(todos);
+  } else if (radioButton_Working.checked) {
+    const taskDoing = todos.filter((todo) => {
+      return todo.status === '作業中';
+    });
+    return showTodos(taskDoing);
+  } else if (radioButton_Done.checked) {
+    const taskDone = todos.filter((todo) => {
+      return todo.status === '完了';
+    });
+    return showTodos(taskDone);
+  }
+}
